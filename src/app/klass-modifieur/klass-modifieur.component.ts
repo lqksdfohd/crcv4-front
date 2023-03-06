9da81b17f5bfda813df675494258d0cf2c4dc3c2
lqksdfohd <22824559+lqksdfohd@users.noreply.github.com>
2023-03-06 14:08:46 +0100

    feat: klass-modifieur est initialisé avec une klass

--- src/app/klass-modifieur/klass-modifieur.component.ts
+++ src/app/klass-modifieur/klass-modifieur.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observer } from 'rxjs';
+import { KlassCompletDto } from '../dtos/klass-complet.dto';
+import { GestionnaireErreursService } from '../gestionnaire-erreurs/gestionnaire-erreurs.service';
+import { KlassModel } from '../models/klass.model';
+import { ProjetBackService } from '../services/projet-back.service';
 
 @Component({
   selector: 'app-klass-modifieur',
@@ -7,9 +13,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class KlassModifieurComponent implements OnInit {
 
-  constructor() { }
+  klassId:string|undefined;
+  klassAModifier:KlassModel|undefined;
+
+  constructor(private activatedRoute:ActivatedRoute, private projetBackService:ProjetBackService,
+  private gestionnaireErreurs:GestionnaireErreursService) {
+    this.activatedRoute.params.subscribe(params => {this.klassId = params['id']});
+  }
 
   ngOnInit(): void {
+    const observeurModifierKlass:Observer<KlassCompletDto> = {
+      next : dto => {
+        this.klassAModifier = new KlassModel();
+        this.klassAModifier.initAvecKlassDtoComplet(dto);
+        console.log(this.klassAModifier);
+      },
+      error: erreur => {this.gestionnaireErreurs.setErreur(erreur.error)},
+      complete: () => {}
+    }
+
+    this.projetBackService.recupererUneKlassDuProjet(this.klassId as string).subscribe(observeurModifierKlass);
   }
 
 }
